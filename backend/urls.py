255a124c7b072bc9ab2c1c01189cc193f3239a24
dan kamau mawaura <mwauradan37@gmail.com>
2026-06-01 11:47:22 +0300

    step-5 dquote> add authtokens application plus JWT tokens

--- backend/urls.py
+++ backend/urls.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 from django.urls import include, path
 
 
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("auth/", include("rest_framework.urls")),
-    path("products/", include("products.urls")),
+    path("products/", include("products.urls", namespace="products")),
+    path("auth/", include("authtokens.urls", namespace="authtokens")),
 ]
